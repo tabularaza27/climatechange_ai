7b83d11178b1aeb7863168c7b7dc145f628a3509
Andrew Ross <andrewslavinross@gmail.com>
2021-07-12 14:39:36 -0400

    Add ICML 2021 papers

--- scripts/convert_subject_areas.rb
+++ scripts/convert_subject_areas.rb
@@ -3,7 +3,7 @@ require 'json'
 
 conversion_table = JSON.parse(File.read(File.join(__dir__, 'tag_conversion_table.json')))
 
-%w(icml2019 neurips2019 iclr2020 neurips2020).each do |w|
+%w(icml2019 neurips2019 iclr2020 neurips2020 icml2021).each do |w|
   yaml_file = "_data/#{w}_papers.yml"
   current = YAML.load(File.read(yaml_file))
   new_data = current.map do |p|
